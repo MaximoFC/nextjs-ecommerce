1c7a323a7172ca4d73af82e2b6f5db32a117db82
MaximoFC <maximofcallejas@gmail.com>
2025-04-18 17:50:23 -0300

    Automatic login upon registration

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import { User } from "@/models/User";
 import { hashPassword } from "@/lib/bcrypt";
+import { generateToken } from "@/lib/jwt";
 
 export async function POST(req: NextRequest) {
     try {
@@ -16,8 +17,23 @@ export async function POST(req: NextRequest) {
         const hashed = await hashPassword(password);
         const newUser = await User.create({ name, email, password: hashed });
 
-        return NextResponse.json({message: 'User registered', user: newUser.email});
-    } catch {
-        return NextResponse.json({error: 'Error registering user'}, {status: 500});
+        const token = generateToken({id: newUser._id, role: newUser.role});
+
+        const response = NextResponse.json({
+            message: 'User registered and logged in',
+            user: newUser.email
+        });
+
+        response.cookies.set('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            path: '/',
+            maxAge: 60 * 60 * 24 * 7
+        })
+
+        return response;
+    } catch (error) {
+        return NextResponse.json({error: 'Error registering user', details: (error as Error).message}, {status: 500});
     }
 }
