78c3a26b6bc3c1e4c9754e1d3d7234d2d17a6063
MaximoFC <maximofcallejas@gmail.com>
2025-04-28 16:15:12 -0300

    Fetch products finished with loader

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,314 +1,59 @@
 'use client';
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MdOutlineArrowForwardIos, MdOutlineArrowBackIos } from "react-icons/md";
 import Link from "next/link";
+import axios from "axios";
+import { HashLoader } from "react-spinners";
+
+interface Size {
+    size: number,
+    stock: number
+}
+
+interface ImageType {
+    url: string,
+    alt: string
+}
+
+interface Product {
+    _id: string,
+    title: string,
+    description: string,
+    brand: string,
+    price: number,
+    category: string,
+    sizes: Size[],
+    images: ImageType[],
+    discount: number,
+    isFeatured: boolean,
+    isActive: boolean
+}
 
 export default function ProductsPage() {
-    const allProducts = [
-        {
-            id: 1,
-            title: "Air Max 90",
-            description: "Zapas para tirar facha",
-            brand: "Nike",
-            price: "$190",
-            category: "Running",
-            sizes: [{
-                size: 42,
-                stock: 2
-            }],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Max 90"
-            }],
-            discount: 0,
-            isFeatured: false,
-            isActive: true
-        },
-        {
-            id: 2,
-            title: "Air Jordan 1 Low",
-            description: "Zapas like Jordan",
-            brand: "Nike",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 3,
-            title: "Air Jordan 1 Low",
-            description: "Zapas like Jordan",
-            brand: "Nike",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 4,
-            title: "Zapa",
-            description: "Zapas like Jordan",
-            brand: "Topper",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 5,
-            title: "Toy",
-            description: "Zapas like Jordan",
-            brand: "Puma",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 6,
-            title: "Clasics",
-            description: "Zapas like Jordan",
-            brand: "Converse",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 7,
-            title: "Superstars",
-            description: "Zapas like Jordan",
-            brand: "Adidas",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 8,
-            title: "Air Jordan 1 Low",
-            description: "Zapas like Jordan",
-            brand: "Nike",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 9,
-            title: "Air Jordan 1 Low",
-            description: "Zapas like Jordan",
-            brand: "Nike",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 10,
-            title: "Air Jordan 1 Low",
-            description: "Zapas like Jordan",
-            brand: "Nike",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 11,
-            title: "Air Jordan 1 Low",
-            description: "Zapas like Jordan",
-            brand: "Nike",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-        {
-            id: 12,
-            title: "Air Jordan 1 Low",
-            description: "Zapas like Jordan",
-            brand: "Nike",
-            price: "$190",
-            category: "Basket",
-            sizes: [
-                {
-                    size: 44,
-                    stock: 3
-                },
-                {
-                    size: 40,
-                    stock: 5
-                }
-            ],
-            images: [{
-                url: "/images/sneakers.png",
-                alt: "Foto de Air Jordan 1 Low"
-            }],
-            discount: 5,
-            isFeatured: true,
-            isActive: true
-        },
-    ]
-
+    const [products, setProducts] = useState<Product[]>([]);
     const [searchTerm, setSearchTerm] = useState("");
     const [selectedCategory, setSelectedCategory] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
+    const [loading, setLoading] = useState(true);
     const itemsPerPage = 10;
 
-    const filteredProducts = allProducts.filter(product => (
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('/api/products');
+                setProducts(response.data);
+            } catch (error) {
+                console.error('Error fetching products', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchProducts();    
+    }, []);
+
+    const filteredProducts = products.filter(product => (
         product.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
         product.brand.toLowerCase().includes(searchTerm.toLowerCase())
     ) &&
@@ -366,74 +111,83 @@ export default function ProductsPage() {
                     </Link>
                 </div>
             </div>
-            <div className="rounded-md border border-zinc-700 overflow-hidden">
-                <table className="w-full table-auto">
-                    <thead className="bg-zinc-700 text-left">
-                        <tr>
-                            <th className="p-4">Imagen</th>
-                            <th className="p-4">Marca</th>
-                            <th className="p-4">Producto</th>
-                            <th className="p-4">Categoría</th>
-                            <th className="p-4">Precio</th>
-                            <th className="p-4">Talles / Stock</th>
-                            <th className="p-4">Activo</th>
-                            <th className="p-4">Descuento</th>
-                            <th className="p-4">Destacado</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {paginatedProducts.map((product) => (
-                            <tr key={product.id} className="border-t border-zinc-700 align-top">
-                                <td className="p-4">
-                                    <Image
-                                        src={product.images[0].url}
-                                        alt={product.images[0].alt}
-                                        width={60}
-                                        height={60}
-                                        className="rounded"
-                                    />
-                                </td>
-                                <td className="p-4">{product.brand}</td>
-                                <td className="p-4">{product.title}</td>
-                                <td className="p-4">{product.category}</td>
-                                <td className="p-4">{product.price}</td>
-                                <td className="p-4">
-                                    <div className="flex flex-col gap-1 items-center">
-                                        {product.sizes.map((s, index) => (
-                                            <span key={index}>{s.size} ({s.stock})</span>
-                                        ))}
-                                    </div>
-                                </td>
-                                <td className="p-4">{product.isActive ? 'Sí' : 'No'}</td>
-                                <td className="p-4">{product.discount}%</td>
-                                <td className="p-4">{product.isFeatured ? 'Sí' : 'No'}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
 
-                {filteredProducts.length === 0 && (
-                    <div className="font-semibold text-center font-4xl p-10">No se encontraron productos</div>
-                )}
-            </div>
-
-            {totalPages > 1  && (
-                <div className="flex justify-center gap-4 mt-4">
-                    <button
-                        onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-                        disabled={currentPage === 1}
-                        className="cursor-pointer bg-green-700 p-4 rounded-lg text-white font-semibold hover:bg-green-900 disabled:bg-zinc-900 disabled:cursor-default"
-                    >
-                        <MdOutlineArrowBackIos />
-                    </button>
-                    <button
-                        onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
-                        disabled={currentPage === totalPages}
-                        className="cursor-pointer bg-green-700 p-4 rounded-lg text-white font-semibold hover:bg-green-900 disabled:bg-zinc-900 disabled:cursor-default"
-                    >
-                        <MdOutlineArrowForwardIos />
-                    </button>
+            {loading ? (
+                <div className="flex justify-center items-center h-96">
+                    <HashLoader size={80} color="#008236" />
                 </div>
+            ) : (
+                <>
+                    <div className="rounded-md border border-zinc-700 overflow-hidden">
+                        <table className="w-full table-auto">
+                            <thead className="bg-zinc-700 text-left">
+                                <tr>
+                                    <th className="p-4">Imagen</th>
+                                    <th className="p-4">Marca</th>
+                                    <th className="p-4">Producto</th>
+                                    <th className="p-4">Categoría</th>
+                                    <th className="p-4">Precio</th>
+                                    <th className="p-4">Talles / Stock</th>
+                                    <th className="p-4">Activo</th>
+                                    <th className="p-4">Descuento</th>
+                                    <th className="p-4">Destacado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {paginatedProducts.map((product) => (
+                                    <tr key={product._id} className="border-t border-zinc-700 align-top">
+                                        <td className="p-4">
+                                            <Image
+                                                src={product.images[0].url}
+                                                alt={product.images[0].alt}
+                                                width={60}
+                                                height={60}
+                                                className="rounded"
+                                            />
+                                            </td>
+                                            <td className="p-4">{product.brand}</td>
+                                            <td className="p-4">{product.title}</td>
+                                            <td className="p-4">{product.category}</td>
+                                            <td className="p-4">${product.price}</td>
+                                            <td className="p-4">
+                                            <div className="flex flex-col gap-1 items-center">
+                                                {product.sizes.map((s, index) => (
+                                                    <span key={index}>{s.size} ({s.stock})</span>
+                                                ))}
+                                            </div>
+                                        </td>
+                                        <td className="p-4">{product.isActive ? 'Sí' : 'No'}</td>
+                                        <td className="p-4">{product.discount}%</td>
+                                        <td className="p-4">{product.isFeatured ? 'Sí' : 'No'}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+
+                        {filteredProducts.length === 0 && (
+                            <div className="font-semibold text-center font-4xl p-10">No se encontraron productos</div>
+                        )}
+                    </div>
+
+                    {totalPages > 1  && (
+                        <div className="flex justify-center gap-4 mt-4">
+                            <button
+                                onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                                disabled={currentPage === 1}
+                                className="cursor-pointer bg-green-700 p-4 rounded-lg text-white font-semibold hover:bg-green-900 disabled:bg-zinc-900 disabled:cursor-default"
+                            >
+                                <MdOutlineArrowBackIos />
+                            </button>
+                            <button
+                                onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+                                disabled={currentPage === totalPages}
+                                className="cursor-pointer bg-green-700 p-4 rounded-lg text-white font-semibold hover:bg-green-900 disabled:bg-zinc-900 disabled:cursor-default"
+                            >
+                                <MdOutlineArrowForwardIos />
+                            </button>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     )
